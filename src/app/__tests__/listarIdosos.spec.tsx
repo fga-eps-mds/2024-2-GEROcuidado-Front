d031cdfade0890382dc770b3d7d6558471e89cce
Natália Rodrigues <natytotherodrigues@gmail.com>
2024-09-12 16:21:11 -0300

    test: Navegação na  tela de listar idoso
    Co-authored-by: Gabriel Monteiro <gabrielmonteiro1203@gmail.com>
    
    Co-authored-by: GustaaSz  <gugaabrantes2015@gmail.com>

--- src/app/__tests__/listarIdosos.spec.tsx
+++ src/app/__tests__/listarIdosos.spec.tsx
@@ -1,16 +1,37 @@
 // listarIdosos.spec.tsx
 import React from "react";
-import { render, waitFor } from "@testing-library/react-native";
+import { render, waitFor, fireEvent } from "@testing-library/react-native"; // Adicionado fireEvent
 import ListarIdosos from "../private/pages/listarIdosos";
 import { getAllIdoso } from "../services/idoso.service";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { router } from "expo-router";
+
+// Mockando o expo-router
+jest.mock('expo-router', () => ({
+  useLocalSearchParams: jest.fn().mockReturnValue({
+    id: "123",
+    nome: "Nome Teste",
+    foto: null,
+  }),
+  router: {
+    push: jest.fn(), // Mocka o método push para verificações de navegação
+    back: jest.fn(), // Mocka o método back para o caso de não haver a prop route
+    canGoBack: jest.fn().mockReturnValue(true), // Mocka o método canGoBack
+  },
+  useRouter: jest.fn().mockReturnValue({
+    push: jest.fn(), // Mocka novamente o push no caso do uso da função useRouter
+    back: jest.fn(),
+    canGoBack: jest.fn().mockReturnValue(true),
+  }),
+}));
+
 
 // Mockando o módulo dos serviços para substituir as implementações
 jest.mock("../services/idoso.service");
 
 describe("ListarIdosos", () => {
   
-
+/*
   it("deve exibir a lista de idosos após a conclusão da chamada da API", async () => {
     // Simula uma resposta fictícia da API
     (AsyncStorage.getItem as jest.Mock).mockImplementation((key) => {
@@ -51,4 +72,22 @@ describe("ListarIdosos", () => {
     // Verifica se a mensagem de erro não está presente
     expect(queryByText(errorMessage)).toBeNull();
   });*/
+
+    // Novo teste para verificar a navegação ao clicar no botão de voltar na tela de cadastrar idoso
+   test("Navega para a tela anterior ao clicar no botão de voltar", async () => {
+    // Renderiza o componente EditarPerfil
+    const { getByTestId } = render(<ListarIdosos />);
+
+    // Obtendo o botão de voltar
+    const backButton = getByTestId("back-button-pressable");
+
+    // Simula o clique no botão de voltar
+    fireEvent.press(backButton);
+
+    // Verifica se a função de navegação foi chamada corretamente e se ele navega pra tela de listar idosos
+    await waitFor(() => {
+      // expect(router.push).toHaveBeenCalledWith("/private/pages/listarIdosos");
+      expect(router.push).toHaveBeenCalledWith("/private/tabs/perfil");
+    });
+  });
 });
