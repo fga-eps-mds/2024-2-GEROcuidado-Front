e79450b284fbcecafa410610e28b9a89d1bccb85
Ugor Marcilio Brandão Costa <ugor_gost@hotmail.com>
2024-09-16 19:47:24 -0300

    Revert "Merge branch 'test/application-new-tests' of https://github.com/fga-eps-mds/2024-1-GEROcuidado-Front into test/application-new-tests"
    This reverts commit 2bc1154760b959ab3ce6cedd1c721d435ffad8a8, reversing
    changes made to a2ca608642e628083a4ad80e318caa29b3dae61c.

--- src/app/private/pages/editarIdoso.tsx
+++ src/app/private/pages/editarIdoso.tsx
@@ -213,7 +213,7 @@ export default function EditarIdoso() {
 
 
    if (!dataNascimento) {
-     erros.dataNascimento = "Campo obrigatório!";
+     erros.dataNascimento = "Campo obrigatório";
    } else if (!/^\d{2}\/\d{2}\/\d{4}$/.test(dataNascimento as string)) {
      erros.dataNascimento = "Data deve ser no formato dd/mm/yyyy!";
    }
