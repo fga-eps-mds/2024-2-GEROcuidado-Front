d9d3b72bd561bde607989136fc30bee17e984b13
Jkluiza <jluiza409@gmail.com>
2024-09-17 17:37:43 -0300

    test: Add test for delete button functionality in VisualizarPublicacao

--- src/app/__tests__/visualizarPublicacao.spec.tsx
+++ src/app/__tests__/visualizarPublicacao.spec.tsx
@@ -1,29 +1,70 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react-native";
+import { render, fireEvent, waitFor, act } from "@testing-library/react-native";
 import VisualizarPublicacao from "../private/pages/visualizarPublicacao";
+import { router } from "expo-router";  // Importa a função de roteamento
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-describe("Visualizar publicacao", () => {
-  it("Rendeziza sem quebrar", () => {
+jest.mock('expo-router', () => ({
+  useLocalSearchParams: jest.fn(() => ({ id: '1' })),
+  router: {
+    push: jest.fn(),
+    replace: jest.fn(),
+  },
+}));
+
+jest.mock("@react-native-async-storage/async-storage", () => ({
+  getItem: jest.fn(),
+}));
+
+interface Mocks {
+  usuario: string;
+  token: string;
+}
+
+describe("VisualizarPublicacao", () => {
+  beforeEach(() => {
+    const mocks: Mocks = {
+      usuario: JSON.stringify({ id: 1, admin: true }),
+      token: "mock-token",
+    };
+
+    (AsyncStorage.getItem as jest.Mock).mockImplementation((key: string) => {
+      return Promise.resolve(mocks[key as keyof Mocks]);
+    });
+  });
+
+  it("Renderiza sem quebrar", () => {
     render(<VisualizarPublicacao />);
   });
-  it("displays actions correctly for admin", () => {
-    const { getByText } = render(<VisualizarPublicacao />, {
-      initialState: {
-        usuario: {
-          id: 1,
-          admin: true,
-        },
-      },
+
+  it("Exibe ações corretamente para admin", async () => {
+    const { getByText } = render(<VisualizarPublicacao />);
+  
+    await waitFor(() => {
+      expect(getByText("Apagar")).toBeTruthy();
+    });
+  });  
+
+  it("Testa botão de apagar", async () => {
+    const { getByTestId, getByText } = render(<VisualizarPublicacao />);
+    
+    // Simulando o clique no botão "Apagar"
+    await act(async () => {
+      fireEvent.press(getByTestId("deleteBtn"));
+    });
+    
+    // Verificando se o modal foi exibido corretamente
+    await waitFor(() => {
+      expect(getByTestId("deleteModal")).toBeTruthy();
+    });
+    
+    // Simulando a confirmação no modal
+    await act(async () => {
+      fireEvent.press(getByTestId("callbackBtn"));
     });
 
-    expect(getByText("Apagar")).toBeTruthy();
-  });
-
-  it("Testa apertar o botao de apagar", () => {
-    const { getByText } = render(<VisualizarPublicacao />);
-    fireEvent.press(getByText("Apagar"));
-
-    // OBS verificar se a lógica de deleção está funcionando corretamente
-    // OBS fazer teste para verificar se o modal de confirmação é exibido após pressionar o botão de deletar.
+  //  await waitFor(() => {
+    //  expect(getByText("Publicação deletada com sucesso")).toBeTruthy();
+    //});
   });
 });
