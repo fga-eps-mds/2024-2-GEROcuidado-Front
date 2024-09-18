50ff11445521a451453ce565aef2e312ee36d5f7
Gustavo Abrantes <gugaabrantes2015@gmail.com>
2024-09-18 13:32:54 -0300

    test: adding some mocks in forum file test

--- src/app/__tests__/forum.spec.tsx
+++ src/app/__tests__/forum.spec.tsx
@@ -6,6 +6,32 @@ import { router } from "expo-router";
 import Toast from "react-native-toast-message";
 import { useNavigation } from '@react-navigation/native';
 
+// Mockando as funções de serviço e navegação
+jest.mock("../services/forum.service");
+jest.mock("expo-router", () => ({
+  router: { push: jest.fn() },
+}));
+jest.mock("react-native-toast-message", () => ({
+  show: jest.fn(),
+}));
+
+// Mock da função de navegação
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: () => ({
+    navigate: jest.fn(),
+  }),
+}));
+
+// Mock para a função que busca publicações
+jest.mock("../services/forum.service", () => ({
+  getAllPublicacao: jest.fn(() => Promise.resolve(mockPublicacoes)),
+}));
+
+const mockPublicacoes = [
+  { id: 1, titulo: "Publicação 1", descricao: "Descrição 1", categoria: "Saúde" },
+  { id: 2, titulo: "Publicação 2", descricao: "Descrição 2", categoria: "Geral" },
+];
+
 describe("Forum", () => {
   it("renderiza corretamente", async () => {
     await waitFor(() => render(<Forum />));
