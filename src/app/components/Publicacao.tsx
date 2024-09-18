1279d01aafca0401b8d0b5d7ba8fe3f2b8df3fb9
Gustavo Abrantes <gugaabrantes2015@gmail.com>
2024-09-18 13:28:44 -0300

    test: adding idUsuarioReporte.length in publicacao file

--- src/app/components/Publicacao.tsx
+++ src/app/components/Publicacao.tsx
@@ -87,14 +87,12 @@ export default function Publicacao({ item, crop }: Readonly<IProps>) {
       </View>
       <Text style={styles.postContent}>{getDescricao(item.descricao)}</Text>
       <View style={styles.underInfo}>
-        {item.idUsuarioReporte.length > 0 && (
-          <View style={styles.reports}>
-            <AntDesing name="warning" size={18} color="#FFCC00" />
-            <Text style={styles.reportsText}>
-              {item.idUsuarioReporte.length}
-            </Text>
-          </View>
-        )}
+      {item.idUsuarioReporte && item.idUsuarioReporte.length > 0 && (
+        <View style={styles.reports}>
+          <AntDesing name="warning" size={18} color="#FFCC00" />
+          <Text style={styles.reportsText}>Usuários reportaram</Text>
+        </View>
+      )}
       </View>
     </Pressable>
   );
