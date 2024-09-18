7c9411e56d798575b30bdc2728991bf8e1cd0530
Gustavo Abrantes <gugaabrantes2015@gmail.com>
2024-09-18 13:30:56 -0300

    test: adding some necessary imports

--- src/app/__tests__/forum.spec.tsx
+++ src/app/__tests__/forum.spec.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-import { render, waitFor } from "@testing-library/react-native";
+import { render, fireEvent, waitFor, act } from "@testing-library/react-native";
 import Forum from "../private/tabs/forum";
+import { getAllPublicacao } from "../services/forum.service";
+import { router } from "expo-router";
+import Toast from "react-native-toast-message";
+import { useNavigation } from '@react-navigation/native';
 
 describe("Forum", () => {
   it("renderiza corretamente", async () => {
