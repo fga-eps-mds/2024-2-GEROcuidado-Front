d5509be665f18d8fd45bff0ba48b7d68c3bc28d1
Natália Rodrigues <natytotherodrigues@gmail.com>
2024-09-20 20:41:48 -0300

    test: when correct call 'callbackFn'
    Co-authored-by: Gabriel Monteiro <gabrielmonteiro1203@gmail.com>
    Co-authored-by: Jessica Luiza Silva de Oliveira <jluiza409@gmail.com>
    Co-authored-by: Marcella Sanderle <marcellasanderle@gmail.com>

--- src/app/__tests__/modalmetrica.spec.tsx
+++ src/app/__tests__/modalmetrica.spec.tsx
@@ -365,6 +365,26 @@ describe("ModalMetrica Component", () => {
         expect(getByText("Formato inválido!")).toBeTruthy(); // Verifica se a mensagem de erro está visível
       });
     });
+
+    it("chama callbackFn com o valor correto ao pressionar Salvar", () => {
+      const mockCallbackFn = jest.fn(); // Cria uma função mock para o callback
+      const { getByTestId } = render(
+        <ModalMetrica
+          visible={true}
+          callbackFn={mockCallbackFn} // Passa a função mock
+          closeModal={() => {}}
+          callbackValor={() => {}}
+          message="Teste"
+          metrica={mockItem}
+        />
+      );
+    
+      const input = getByTestId("valorInput"); // Obtém o input pelo testID
+      fireEvent.changeText(input, "123"); // Define um valor válido
+      fireEvent.press(getByTestId("callbackBtn")); // Pressiona o botão Salvar
+    
+      expect(mockCallbackFn).toHaveBeenCalledWith("123"); // Verifica se a função foi chamada com o valor correto
+    });
   });
 });
 
