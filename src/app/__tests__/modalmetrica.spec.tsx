250db3ac4038d05e9201485134f7116dcaf3cfbe
Natália Rodrigues <natytotherodrigues@gmail.com>
2024-09-20 20:35:40 -0300

    test: error message invalid value
    Co-authored-by: Gabriel Monteiro <gabrielmonteiro1203@gmail.com>
    Co-authored-by: Jessica Luiza Silva de Oliveira <jluiza409@gmail.com>
    Co-authored-by: Marcella Sanderle <marcellasanderle@gmail.com>

--- src/app/__tests__/modalmetrica.spec.tsx
+++ src/app/__tests__/modalmetrica.spec.tsx
@@ -342,6 +342,29 @@ describe("ModalMetrica Component", () => {
         expect(getByText("Campo obrigatório!")).toBeTruthy();
       });
     });
+
+    it("exibe mensagem de erro ao tentar salvar com formato inválido", async () => {
+      const { getByTestId, getByText } = render(
+        <ModalMetrica
+          visible={true}
+          callbackFn={() => {}}
+          closeModal={() => {}}
+          callbackValor={() => {}}
+          message="Teste"
+          metrica={mockItem}
+        />,
+      );
+    
+      // Supondo que haja um campo de entrada para o valor
+      const input = getByTestId("valorInput"); // Altere para o seu testID real do input
+      fireEvent.changeText(input, "abc123"); // Valor inválido
+    
+      fireEvent.press(getByTestId("callbackBtn")); // Pressiona o botão para salvar
+    
+      await waitFor(() => {
+        expect(getByText("Formato inválido!")).toBeTruthy(); // Verifica se a mensagem de erro está visível
+      });
+    });
   });
-  
 });
+
